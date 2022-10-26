4125b671cfacfdd578fd6181504eab8fbd566e8c
jcmsuperstar <coreiconid@gmail.com>
2022-10-26 05:30:05 -0600

    1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 M.AutoInit();
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instance = M.Collapsible.getInstance(elem);
+var elem = document.querySelector('.collapsible.expandable');
+var instance = M.Collapsible.init(elem, {
+  accordion: false
 });
